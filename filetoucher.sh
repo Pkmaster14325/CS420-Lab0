a2ca504eac02c696ecb29b3944a27f76b122f938
Tanner Dudley <dudley.tanner@yahoo.com>
2025-10-03 22:38:30 -0500

    added comments before submitting

--- filetoucher.sh
+++ filetoucher.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 #
-#Shell Script that will check if an inputted file exists in the current directory and adds it to a list 
-#that will be printed out. If it doesn't create that file.
-#If it does, we will overwrite that file and print it out.
+#Shell Script that tracks all files in the current directory
+#in a new tracking file of whatever you name it. If the file exists, overwrite it. Otherwise 
+#Create the new file.
+
 
 echo "File name: $1" 
 
+#Store the file name that is passed when running the script
 FILE=$1
 
+#We check if that file exists, if it exists overwrite it. 
 if [ -e $FILE ]; then
 >$FILE
 
 else
-
+#Create the new file otherwise
 touch $FILE
 
 fi
-
+#Looop through all files in the directory, append each filename to the tracking file
 for f in *; do
 if [ -f "$f" ]; then
 echo "$f" >> "$FILE"
 fi
 done
 
+#Print the contents of the file
 cat "$FILE"
-
-
-
